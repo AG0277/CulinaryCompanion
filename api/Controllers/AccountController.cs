c00cb6f6f5384829e973b9fbaf4083fd7fe6ad07
AG0277 <grabek.artur19@gmail.com>
2024-03-18 21:06:05 +0100

    added JWT, created User

---
+++ api/Controllers/AccountController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Account;
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace api.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly ITokenService tokenService;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(
+            UserManager<AppUser> UserManager,
+            ITokenService TokenService,
+            SignInManager<AppUser> SignInManager
+        )
+        {
+            userManager = UserManager;
+            tokenService = TokenService;
+            signInManager = SignInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                var appUser = new AppUser
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email
+                };
+                var createdUser = await userManager.CreateAsync(appUser, registerDto.Password);
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await userManager.AddToRoleAsync(appUser, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok(
+                            new NewUserDto
+                            {
+                                Username = appUser.UserName,
+                                Email = appUser.Email,
+                                Token = tokenService.CreateToken(appUser)
+                            }
+                        );
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto LoginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var user = await userManager.Users.FirstOrDefaultAsync(x =>
+                x.UserName == LoginDto.Username.ToLower()
+            );
+            if (user == null)
+                return Unauthorized("Invalid username");
+            var result = await signInManager.CheckPasswordSignInAsync(
+                user,
+                LoginDto.Password,
+                false
+            );
+            if (!result.Succeeded)
+                return Unauthorized("Invalid credentials");
+            return Ok(
+                new NewUserDto
+                {
+                    Username = user.UserName,
+                    Email = user.Email,
+                    Token = tokenService.CreateToken(user)
+                }
+            );
+        }
+    }
+}
---
+++ api/Services/TokenService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace api.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration configuration;
+        private readonly SymmetricSecurityKey key;
+
+        public TokenService(IConfiguration Configuration)
+        {
+            configuration = Configuration;
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SigningKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.GivenName, user.UserName)
+            };
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
+                Issuer = configuration["JWT:Issuer"],
+                Audience = configuration["JWT:Audience"]
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
